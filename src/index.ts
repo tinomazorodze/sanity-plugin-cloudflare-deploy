75c11d4a05fd83ec6334efdc20ed0d68917ef8aa
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2026-08-26 18:34:58 +0200

    export type fix

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
 export { cloudflareDeployTool } from "./deploy-tool";
-export { deploymentSchema } from "./schemas/deployment";
-export {
-  triggerDeployHook,
-  triggerCloudflareDeploy,
-  listCloudflareDeployments,
-  getCloudflareDeployment,
-  mapCloudflareStatusToDeploymentStatus,
-  findMatchingDeployment,
-} from "./lib/cloudflare";
-export { normalizeConfig, validateConfig } from "./lib/config";
-export { DeployPage } from "./components/deploy-page";
-export { Icons } from "./components/icons";
 
-export type {
-  CloudflareDeployToolConfig,
-  CloudflareDeployTarget,
-  CloudflareApiConfig,
-  DeploymentRecord,
-  DeploymentStatus,
-  DeploymentTrigger,
-  DeploymentEnvironment,
-  DeploymentLog,
-  DeploymentUser,
-  CloudflarePagesDeployment,
-  CloudflarePagesStage,
-  NormalizedDeployTarget,
-} from "./types";
+export type { CloudflareDeployToolConfig } from "./types";
